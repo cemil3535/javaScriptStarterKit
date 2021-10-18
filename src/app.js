03cb4aa6ae234f613fe68e5bf15f9b7db4759f97
cemil3535 <cemilozcan35@gmail.com>
2021-10-19 01:11:02 +0300

    18 Ekim Ders

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+console.log(dolarBugun)
+
+//array
+let konutKredileri= ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+console.log("Merhaba Dünya")
